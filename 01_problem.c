a7c3146c25288b12ee86b0b27887b85ec83b4a40
JatinSangwan01 <jatin0758x@gmail.com>
2024-07-20 21:22:44 +0530

    Initial commit

---
+++ 01_problem.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int price, ltrs;
+printf("Enter price:");
+scanf("%d", &price);
+
+printf("Enter ltrs:");
+scanf("%d", &ltrs);
+
+printf("Total price of petroling is %d", price*ltrs);
+     return 0;
+}
---
+++ 04.problem.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    float a = 8.0 +2;
+    printf("The value of a is %f",a);
+     return 0;
+}
