d9fca01eb0db414127b597970475ccbae23fa5cc
JatinSangwan01 <jatin0758x@gmail.com>
2024-09-01 20:30:52 +0530

    Add files via upload

---
+++ 52_2D_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int arr[4][2] = {{1, 2}
+                     ,{3, 4}
+                     ,{5, 6}
+                     ,{7, 8}};
+
+    printf("the value of [3][0] is %d and %d",arr[2][0], arr[1][1]);
+   
+
+    return 0;
+}
